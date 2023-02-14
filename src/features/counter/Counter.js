8e04bf8e6f97832845c1674508eab7aa6fa6a1a2
EunjiLee1997 <eunjilee1111@gmail>
2023-02-14 14:46:04 +0900

    styled component 및 redux 적용 실습

---
+++ src/features/counter/Counter.js
@@ -0,0 +1,39 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from './counterSlice';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  display: inline-block;
+  color: palevioletred;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+  display: block;
+`;
+
+export function Counter() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div>
+        <Button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </Button>
+        {count}
+        <Button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </Button>
+      </div>
+    </div>
+  );
+}
